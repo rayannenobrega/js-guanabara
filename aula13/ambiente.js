447b71a992bc97886fe03bec10d861de4fb22952
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-31 19:42:51 -0300

    Repetições parte 01

---
+++ aula13/ambiente.js
@@ -0,0 +1,13 @@
+//Laço Simples - Teste Lógico no início
+var c = 1;
+while( c <= 6){
+    console.log(`Passo ${c}`);
+    c++;
+}
+
+//Teste lógico no final
+var contador = 1;
+do {
+    console.log(`Passo ${contador}`);
+    contador++
+} while (contador <=6)
